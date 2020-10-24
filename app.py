d130a957a81711839a43a3d61d1e5958a0624ea6
gala <gal.aviv@cellwize.com>
2020-10-24 14:16:32 +0300

    progress with spotiflask

--- app.py
+++ app.py
@@ -1,12 +1,40 @@
+import base64
 from flask import Flask, request, jsonify, render_template
+from spotify_client import *
+from secret import *
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return render_template(
       'index.html'
     )
+
+@app.route('/about')
+def about():
+    return render_template(
+        'about.html'
+    )
+
+@app.route('/search')
+def search():
+    try:
+      query = request.args['user_query']
+      search_type = request.args['search_type']
+      print('search type', search_type)
+      spotify = SpotifyAPI(client_id, client_secret)
+      results = spotify.search(query, search_type = search_type)[search_type + 's']['items']
+      print(results)
+      return render_template(
+          'search.html',
+          results=results,
+          search_type=search_type
+      )
+    except Exception as e:
+      print('blahh', e)
+      return render_template('search.html')
 
 if __name__ == '__main__':
     app.run(threaded=True, port=5000)
---
+++ secret.py
@@ -0,0 +1,2 @@
+client_id = "785f4d840d3d484b8dca218ba95726be"
+client_secret = "afe53e787674400e83433fc62f0cf75d"
